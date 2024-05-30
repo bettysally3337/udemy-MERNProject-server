8a0b7887efe69a6e44f09721852df77000c3f494
Lexi Cheng <girlmeetsworld.cys@gmail.com>
2024-05-30 13:41:05 +0800

    fix cors

--- index.js
+++ index.js
@@ -23,6 +23,12 @@ mongoose
 //middlewawre
 app.use(express.json());
 app.use(express.urlencoded({ extended: true }));
+app.use((req, res, next) => {
+  res.setHeader("Access-Control-Allow-Origin", "*");
+  res.setHeader("Access-Control-Allow-Methods", "POST, GET, PUT");
+  res.setHeader("Access-Control-Allow-Headers", "Content-Type");
+  next();
+});
 app.use(cors());
 
 console.log("進入authRoute之前");
